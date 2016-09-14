17d3447c306d8b1b013d90920b04b6d8ea3bcac8
Oron Nadiv <oron@nadiv.us>
2016-09-13 23:44:30 -0700

    updating dependencies

--- scripts/garage.sh
+++ scripts/garage.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This file starts the raspberry pi client.
 # Follow installation instructions in the README.md file.
 # Then place this file in the user's root directory.
@@ -6,11 +8,11 @@ cd ~/garage-door-raspberry-client
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use
-export PRIVATE_KEY=<PASTE GENERATED PRIVATE KEY>
+export PRIVATE_KEY= # PASTE GENERATED PRIVATE KEY
 export NODE_ENV=production
-export LOGIN_URL=https://<LOGIN_SERVER_URL>
-export PINS_OPEN_DOOR_SIGNAL=<GPIO PIN FOR DOOR REMOTE>
-export PINS_READ_DOOR_STATE=<GPIO PIN FOR DOOR STATE (OPEN/CLOSE)>
-export PUSH_URL=https://<PUSH_SERVER_URL>
-export SERVER_URL=https://<GARAGE_SERVER_URL>
+export LOGIN_URL= # https://<LOGIN_SERVER_URL>
+export PINS_OPEN_DOOR_SIGNAL= # <GPIO PIN FOR DOOR REMOTE>
+export PINS_READ_DOOR_STATE= # <GPIO PIN FOR DOOR STATE (OPEN/CLOSE)>
+export PUSH_URL= # https://<PUSH_SERVER_URL>
+export SERVER_URL= # https://<GARAGE_SERVER_URL>
 npm start
